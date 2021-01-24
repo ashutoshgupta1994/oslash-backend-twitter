1d959848dade86b94c3b251a61bea47cf825cae1
ashutoshgupta1994 <64656721+ashutoshgupta1994@users.noreply.github.com>
2021-01-24 23:22:42 +0530

    Update config.js
    added url for mlab cluster

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports ={
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://127.0.0.1:27017/oslash-twitter',
+    'mongoUrl': 'mongodb+srv://oslash:Oslash@2021@cluster0.m3oy2.mongodb.net/oslash-twitter?retryWrites=true&w=majority',
     'roleDef' : {"role_user" : ["user_tweet_create", "user_tweet_edit", "user_tweet_read", "user_tweet_delete"],
                 "role_admin" : ["user_tweet_create", "user_tweet_edit", "user_tweet_read", "user_tweet_delete",
                                 "admin_tweet_create", "admin_tweet_edit", "admin_tweet_read", "admin_tweet_delete",
